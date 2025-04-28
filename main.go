d8745f4227ff85713dbeba1d3256fc2fb103132d
che711 <che7111987@gmail.com>
2025-04-28 07:58:11 +0200

    updated main.go

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
 	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("\nHello and welcome, %s!\n", s)
+	fmt.Print("Many\n")
+	fmt.Print("Thanks\n")
+	fmt.Print("To")
+	fmt.Print("All")
+	fmt.Print("My")
+	fmt.Print("Students")
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	fmt.Print("\n")
+	fmt.Println("Hello", "my", "dear", "friends")
 }
