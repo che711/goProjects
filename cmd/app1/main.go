35264868f771221c93fb0e49ddd244df1924ebb7
che711 <che7111987@gmail.com>
2025-05-06 08:10:37 +0200

    updated main.go in app1

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -27,38 +27,65 @@ func main() {
 	//
 	//fmt.Print("\n")
 	//fmt.Println("Hello", "my", "dear", "friends")
-	//
-	//fmt.Println("*")
-	//fmt.Println("**")
-	//fmt.Println("***")
-	//fmt.Println("****")
-	//fmt.Println("*****")
-	//fmt.Println("******")
-	//fmt.Println("*******")
 
-	// 1 вариант
-	const a = "строка"
-	//a = 'test_msg'
-	fmt.Println(a)
+	/*
+		// 1 вариант
+		const a = "строка"
+		//a = 'test_msg'
+		fmt.Println(a)
 
-	// 2 вариант
-	var b, c, d string // пустая строка ""
-	fmt.Println(b, c, d)
+		// 2 вариант
+		var b, c, d string // пустая строка ""
+		fmt.Println(b, c, d)
 
-	// 3 вариант
-	f, e := "new_test", "test"
+		// 3 вариант
+		f, e := "new_test", "test"
+		fmt.Println(e)
+		fmt.Println(f)
+	*/
+
+	var text string = "string"
+	fmt.Println(text)
+
+	// обратите внимание на одинарные кавычки
+	var symbol rune = 'A'
+	fmt.Println(symbol)
+
+	var b int = 123123123
+	fmt.Println(b)
+	var c int8 = 123
+	fmt.Println(c)
+	var d int16 = 1234
+	fmt.Println(d)
+	var e int32 = 123123123
 	fmt.Println(e)
+	var f int64 = 123123123123
 	fmt.Println(f)
 
-	var someText string = ""
-	fmt.Println("some text: ", someText)
+	var g uint = 999
+	fmt.Println(g)
+	var h uint8 = 100
+	fmt.Println(h)
+	var i uint16 = 1000
+	fmt.Println(i)
+	var j uint32 = 10000
+	fmt.Println(j)
+	var k uint64 = 10000000
+	fmt.Println(k)
 
-	someText = "new text"
-	fmt.Println("some text: ", someText)
+	var l float32 = 1000.0
+	fmt.Println(l)
+	var m float64 = 9999.123123123
+	fmt.Println(m)
 
-	moreText := someText
-	fmt.Println("more text: ", moreText)
+	var n complex64 = 100 + 100i
+	fmt.Println(n)
+	var o complex128 = 1000 + 1000i
+	fmt.Println(o)
 
-	fmt.Println("")
+	var yes bool = true
+	fmt.Println(yes)
+	var no bool = false
+	fmt.Println(no)
 
 }
