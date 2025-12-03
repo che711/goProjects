70d32111011396240e253a1a618d0083dc1f245c
che711 <che7111987@gmail.com>
2025-12-03 07:26:51 +0100

    update main.go

--- internal/utils/code.go
+++ internal/utils/code.go
@@ -269,16 +269,16 @@ func IfConditions(){
 }
 
 func IfElseConditions() {
-	log.Println("\n\n\tThis is a IfElseConditions function")	
-    log.Printf("Форматированное сообщение: %d", 42)
+	log.Println("\n\n\tThis is a IfElseConditions function\n")	
+	log.Printf("\n")	
 	// С флагом для добавления даты/времени
     log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-    log.Println("Сообщение с метаданными")
-
+    log.Println("Msg with metadata")
+	
 	// Запись в файл
     file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
     if err != nil {
-        log.Fatal(err)
+		log.Fatal(err)
     }
     defer file.Close()
     
@@ -286,14 +286,15 @@ func IfElseConditions() {
     log.Println("Запись в файл")
     
     // Fatal выводит сообщение и завершает программу
-    // log.Fatal("Критическая ошибка")
+    log.Fatal("Критическая ошибка")
     
     // Panic выводит сообщение и вызывает panic
-    // log.Panic("Паника")
+    log.Panic("Паника")
+	
+	var age int = 34
+	log.Printf("Форматированное сообщение: %d", age)
 
-	var age int
-
-	fmt.Scan(&age)
+	// fmt.Scan(&age)
 	if age <13 {
 		fmt.Println("You are a little yet")
 	} else if age < 18 {
