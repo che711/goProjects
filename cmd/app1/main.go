170bdfbdeecbd6a774674af8e581c5335252766e
che711 <che7111987@gmail.com>
2026-01-25 09:56:35 +0100

    updated the code.go

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"goProjects/internal/utils"
-	)
+)
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
@@ -43,5 +43,5 @@ func main() {
 	// utils.IfConditions()
 	// utils.IfElseConditions()
 	utils.Logger()
-	
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module goProjects
 
-go 1.24
+go 1.22
